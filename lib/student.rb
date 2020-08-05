ca29271a3f28cd481b88439bedbb970f1edfd882
Jenise Ruehle <jela07@gmail.com>
2020-08-05 19:17:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,7 +55,7 @@ class Student
     new_student
   end
   
-  def find_by_name(name)
+  def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
     Student.new(result[0], result[1], result[2])
